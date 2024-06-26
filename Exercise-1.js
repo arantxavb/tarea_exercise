a0cdbc2e1503d730d8fc1e422fb76622171b9010
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-06-26 10:52:59 -0600

    añadi cada archivo con cada tarea

---
+++ Exercise-1.js
@@ -0,0 +1,12 @@
+
+////////////////////    JOIN ////////////////////
+
+
+console.log("bievenidos");
+
+let arr = ["This", "is", "a", "sentence."];
+function printOutString() {
+  console.log(arr.join(" "))
+}
+ console.log(arr);
+
---
+++ exercise-2.js
@@ -0,0 +1,46 @@
+/////////////////////// MAP ///////////////////
+/*
+
+Este método crea un nuevo array con los resultados de aplicar 
+una función a cada uno de los elementos del array original. 
+Es una herramienta muy poderosa para transformar datos 
+de manera funcional y concisa.
+Sintaxis:
+arr.map(function callback(currentValue, index, array)
+*/
+ ///////////////forma extendida////////////////////////////
+const duplicarEdad = ( currentValue, index, array )=>{
+    return currentValue * 2;
+}
+const ages = [1, 2, 4, 10, 15] ;
+const dobleAges = ages.map(  duplicarEdad );
+
+//////////////////////sintetizada/////////////////////////
+
+let simpleArray = [4, 8, 12, 24, 32];
+//const doubleArray = simpleArray.map((x)=> x*2);
+
+const doubleArray = simpleArray.map((x)=> x*2);
+
+console.log(doubleArray);
+
+
+
+
+
+// [1, 2, 4, 10, 15] 
+const duplicarValor = ( array ) => {
+    const newArray = [];
+    for(let i= 0; i<array.length; i++ ){
+      newArray.push( array[i] * 2 );
+    }
+    return newArray;
+ };
+ const myArray = [1, 2, 4, 10, 15] ;
+ console.log(  duplicarValor( myArray )   ); // [2, 4, 8, 20, 30]
+
+
+
+
+
+
